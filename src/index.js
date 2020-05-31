653ed8eb1999aee619b21bcffb21d472b04d5749
Nykollas <nicolauarti09@gmail.com>
2020-05-31 02:29:26 -0300

    template adicionado

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//import './App.css';
+import './assets/scss/style.scss';
+
+const history = createBrowserHistory();
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Router history={history}>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
